874a5306155ab92c8a37f3c216b3a7f42041e491
Amr Al-odini <160176809+43k6@users.noreply.github.com>
2026-04-14 18:15:57 +0300

    Add Mapping files

---
+++ FinGuardAI.Business/Mappings/FinancialResponseProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using FinGuardAI.DataAccess.Entities;
+using static FinGuardAI.DataAccess.DTOs.FinancialResponseDTO;
+
+public class FinancialResponseProfile : Profile
+{
+    public FinancialResponseProfile()
+    {
+        // من Entity إلى DTO
+        CreateMap<FinancialResponse, FinancialResponseDto>()
+            .ForMember(dest => dest.CreatorName, opt => opt.MapFrom(src => src.Creator.UserName));
+
+        // من DTO إلى Entity
+        CreateMap<FinancialResponseDto, FinancialResponse>()
+            .ForMember(dest => dest.Request, opt => opt.Ignore())
+            .ForMember(dest => dest.Creator, opt => opt.Ignore())
+            .ForMember(dest => dest.Id, opt => opt.Ignore());
+    }
+}
---
+++ FinGuardAI.Business/Mappings/UserProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using FinGuardAI.DataAccess.DTOs;
+using FinGuardAI.DataAccess.Entities;
+
+namespace FinGuardAI.Business.Mappings
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserDTO>().ReverseMap();
+
+        }
+    }
+
+
+}
