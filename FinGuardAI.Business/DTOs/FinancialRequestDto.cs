a743552f4e1d6e5c769078133a1e2d2438cb2fa9
Amr Al-odini <160176809+43k6@users.noreply.github.com>
2026-04-14 18:14:46 +0300

    Add Dtos

---
+++ FinGuardAI.Business/DTOs/FinancialRequestDto.cs
@@ -0,0 +1,33 @@
+﻿namespace FinGuardAI.DataAccess.DTOs
+{
+    public partial class FinancialResponseDTO
+    {
+        // DTO للـ FinancialRequest
+        public class FinancialRequestDto
+        {
+            public int Id { get; set; }
+            public string RequestName { get; set; }
+            public decimal Amount { get; set; }
+            public string Category { get; set; }
+            public string Description { get; set; }
+            public string State { get; set; }
+            public DateTime CreatedAt { get; set; }
+            public string CreatorName { get; set; }
+            public FinancialRequestDto() { }
+
+            public FinancialRequestDto(int id, string requestName, decimal amount, string category,
+                                       string description, string state, DateTime createdAt, string creatorName)
+            {
+                Id = id;
+                RequestName = requestName;
+                Amount = amount;
+                Category = category;
+                Description = description;
+                State = state;
+                CreatedAt = createdAt;
+                CreatorName = creatorName;
+            }
+        }
+    }
+}
+
---
+++ FinGuardAI.Business/DTOs/FinancialResponseDto.cs
@@ -0,0 +1,32 @@
+﻿namespace FinGuardAI.DataAccess.DTOs
+{
+    public partial class FinancialResponseDTO
+    {
+        // DTO للـ FinancialResponse
+        public class FinancialResponseDto
+        {
+            public int Id { get; set; }
+            public int RequestId { get; set; }
+            public string Decision { get; set; }
+            public decimal AcceptedAmount { get; set; }
+            public string Justification { get; set; }
+            public DateTime CreatedAt { get; set; }
+            public string CreatorName { get; set; }
+
+          public  FinancialResponseDto() { }
+
+            public FinancialResponseDto(int id, int requestId, string decision, decimal acceptedAmount,
+                                        string justification, DateTime createdAt, string creatorName)
+            {
+                Id = id;
+                RequestId = requestId;
+                Decision = decision;
+                AcceptedAmount = acceptedAmount;
+                Justification = justification;
+                CreatedAt = createdAt;
+                CreatorName = creatorName;
+            }
+        }
+    }
+}
+
